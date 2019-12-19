423ea9991536f21e2880c7ca439e37daefbb52e1
m4merg <maxim.ivanov@atlas.ru>
2019-12-19 17:52:53 +0300

    work

--- HF_grep_var_count.pl
+++ HF_grep_var_count.pl
@@ -23,7 +23,7 @@ use lib __DIR__ . '/lib';
 use Score;
 use Cigar;
 use Sample;
-
+use Design;
 
 $| = 1;
 
@@ -182,67 +182,6 @@ sub select_amplicon {
 	return $selection;
 	}
 
-sub define_amplicons {
-	my $inputFile	= shift;
-	my $header	= shift;
-	open (my $panel_fh, "<$inputFile") or die "Can not open panel file\n";
-	
-	my $amplicon;
-	while (<$panel_fh>) {
-		my @mas = split/\t/;
-		next unless defined $mas[2];
-		next unless $mas[1] =~ /^\d+$/;
-		next unless $mas[2] =~ /^\d+$/;
-		die "Unknown contig name $mas[0] in BED file. Non-concordant with input BAM file\n" unless defined $header->{$mas[0]};
-		die "Segment end position ($mas[2]) out of contig length for chromosome $mas[0]\n" if $mas[2] > $header->{$mas[0]};
-		for (my $i = $mas[1] + 1; $i <= $mas[2]; $i++) {
-			my $name = "$mas[0]:$i";
-			$amplicon->{$name} = [] unless defined($amplicon->{$name});
-			push @{$amplicon->{$name}}, "$mas[0]:$mas[1]-$mas[2]";
-			}
-		}
-
-	close $panel_fh;
-	return $amplicon;
-	}
-
-sub define_segments { # simple merge BED file
-	my $inputFile	= shift;
-	my $header	= shift;
-	open (my $panel_fh, "<$inputFile") or die "Can not open panel file\n";
-
-	my $segments;
-	my $current_segment;
-	while (<$panel_fh>) {
-		my @mas = split/\t/;
-		next unless defined $mas[2];
-		next unless $mas[1] =~ /^\d+$/;
-		next unless $mas[2] =~ /^\d+$/;
-		die "Unknown contig name $mas[0] in BED file. Non-concordant with input BAM file\n" unless defined $header->{$mas[0]};
-		die "Segment end position ($mas[2]) out of contig length for chromosome $mas[0]\n" if $mas[2] > $header->{$mas[0]};
-		unless (defined $current_segment) {
-			$current_segment = {contig => $mas[0], start => $mas[1], end => $mas[2], mutations => []};
-			next;
-			}
-		if (($mas[0] eq $current_segment->{contig}) and ($mas[1] < $current_segment->{start})) {
-			die "Input BED file is not sorted at position $mas[0]:$mas[1] <-; Use \"sort -k1,1 -k2,2n <input_bed>\" to sort bed file\n";
-			}
-		if (($mas[0] eq $current_segment->{contig})
-				and ($mas[1] > $current_segment->{start})
-				and ($mas[1] < $current_segment->{end})) {
-			if ($mas[2] > $current_segment->{end}) {
-				$current_segment->{end} = $mas[2];
-				} else {
-				next;
-				}
-			} else {
-			push (@{$segments}, $current_segment);
-			$current_segment = {contig => $mas[0], start => $mas[1], end => $mas[2], mutations => []};
-			}
-		}
-	return $segments;
-	}
-
 sub make_mutation_connection {
 	my $mutation;
 	
@@ -372,9 +311,10 @@ sub head {
 	my $vcfFile	= $ARGV[2];
 	my $Sample	= Sample->new($inputBam);
 	
+	my $Design = Deign->new;
+	$Design->loadSeqDic($Sample->header);
+	$Design->loadPanel($panelFile);
 
-	my $segments		= define_segments($panelFile, $Sample->header);
-	my $ampliconsHash	= define_amplicons($panelFile, $Sample->header);
 	my $mutations		= load_mutations($vcfFile, $Sample->header);
 	die "Can not identify genome regions in input BED file ($panelFile)\n" unless defined $segments;
 	die "Can not identify genome regions in input BED file ($panelFile)\n" unless defined $ampliconsHash;
